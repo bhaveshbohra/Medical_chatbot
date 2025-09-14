b9d373081dfc836c2895a03fe5c3bd1af81a28c2
Bhavesh Bohra <bhaveshb.995@yahoo.com>
2025-09-14 09:02:34 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+## creating directiory : using .sh
+mkdir -p src
+mkdir -p research 
+
+touch src/__init__.py 
+touch src/helper.py
+touch src/prompt.py
+
+touch .env
+touch .env.example
+
+touch setup.py
+touch app.py
+touch research/experiment1.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully!."
